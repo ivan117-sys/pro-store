44eecf1033bc23139d5e59a209ada1a97c7d85be
Ivan Mušković <ivanmuskovic@gmail.com>
2025-02-06 18:22:41 +0100

    fixes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 'use server';
 
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import  { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 // Sign in user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -18,6 +21,8 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
     return { success: true, message: 'Signed in successfully' }
     
   } catch (error) {
+
+
     if (isRedirectError(error)) {
       throw error;
     }
@@ -30,3 +35,46 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 export async function signOutUser() {
   await signOut();
 }
+
+// Sign up user
+export async function signUpUser(prevState: unknown, formData: FormData) { 
+
+
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    })
+
+    
+    const plainPasword = user.password
+    
+    user.password = hashSync(user.password, 10);
+    
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password
+      }
+    })
+    
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPasword
+    })
+    
+    return { success: true, message: 'User registered successfully' }
+    
+  } catch (error) {
+  
+    if (isRedirectError(error)) {
+      throw error;
+    }
+   
+    return { success: false, message: formatError(error) };
+
+  }
+ }
